a19e124e241fa0e07fa8a7501cc1c1cdf3158bab
Marco Servetto <marco.servetto@gmail.com>
2026-05-01 08:39:55 +1200

    testing txt input encodings

---
+++ rt/_AssetReadCapability$0.java
@@ -0,0 +1,11 @@
+package base;
+
+import static base.Util.*;
+
+import java.util.ArrayList;
+
+public final class _AssetReadCapability$0 implements AssetReadCapability$0{
+  @Override public final Object mut$iso$0(){ return this; }
+  @Override public final Object mut$close$0(){ return this; }
+}
+//Should move the _TxtRead.readXX methods in here
--- rt/_System$0.java
+++ rt/_System$0.java
@@ -2,8 +2,12 @@ package base;
 import java.util.*;
 
 public final class _System$0 implements System$0{
+  @Override public final Object mut$iso$0(){ return this; }
+  @Override public final Object mut$close$0(){ return this; }
   public Object mut$try$0(){ return new _CapTry$0(); }
   public Object mut$out$0(){ return new _Output$0(); }
   public Object mut$inputCursor$0(){ return new _InputCursor$0(); }
-  public Object mut$readLog$1(Object inMemLog){ return new _LogReader$1((InMemoryLog$1)inMemLog,new ArrayList<Object>()); }
+  public Object mut$readLog$1(Object inMemLog){ 
+    return new _LogReader$1((InMemoryLog$1)inMemLog,new ArrayList<Object>()); }
+  public Object mut$assetRead$0(){ return new _AssetReadCapability$0(); }
 }
